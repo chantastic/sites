1638526b3c6b953b93d81297c8d639882678573a
Michael Chan <mijoch@gmail.com>
2018-10-16 16:39:17 -0700

    setup MDX and add old content

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+// const images = require('remark-images')
+// const emoji = require('remark-emoji')
+let highlight = require("remark-highlight.js");
+
 const withMDX = require("@zeit/next-mdx")({
-  extension: /.mdx?$/
+  extension: /.mdx?$/,
+  options: {
+    mdPlugins: [highlight]
+  }
 });
 
 module.exports = withMDX({
