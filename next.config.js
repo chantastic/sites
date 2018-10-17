d046050139012957889d1e5c9b1e6b5b697075b7
Michael Chan <mijoch@gmail.com>
2018-10-16 17:01:00 -0700

    add md slugs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 // const images = require('remark-images')
 // const emoji = require('remark-emoji')
-let highlight = require("remark-highlight.js");
-let withCSS = require("@zeit/next-css");
+let mdPlugins = [
+  require("remark-slug"),
+  require("remark-highlight.js")
+];
 
+let withCSS = require("@zeit/next-css");
 let withMDX = require("@zeit/next-mdx")({
   extension: /.mdx?$/,
   options: {
-    mdPlugins: [highlight]
+    mdPlugins
   }
 });
 
