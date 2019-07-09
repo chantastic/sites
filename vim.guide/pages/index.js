67c041bef2e72d2341302fbc5fa10d7802879f87
Michael Chan <mijoch@gmail.com>
2019-07-08 17:14:46 -0700

    vim.guide: remove link to about

--- vim.guide/pages/index.js
+++ vim.guide/pages/index.js
@@ -1,16 +1,12 @@
 import Link from "next/link";
 import Header from "../components/header";
 import AlphabetPage from "./alphabet/index";
-import "system-ui.css"
+import "system-ui.css";
 
 export default () => (
   <main className="system-ui">
     <Header />
-    <section>
-      <Link href="/about">
-        <a>Go to About <span>Me</span></a>
-      </Link>
-    </section>
+
     <section>
       <h2>Alphabet (Normal)</h2>
       <AlphabetPage />
