1aeb2573695ce664c1be6d42b56a4e66d97c773c
Michael Chan <mijoch@gmail.com>
2019-07-19 14:26:33 -0700

    chan.dev: setup /blog with gatsby-theme-blog

--- chan.dev/gatsby-config.js
+++ chan.dev/gatsby-config.js
@@ -4,8 +4,31 @@ module.exports = {
       resolve: `gatsby-theme-notes`,
       options: {},
     },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    },
   ],
   siteMetadata: {
-    title: `Site title`,
+    // Used for the site title and SEO
+    title: `My Blog Title`,
+    // Used to provide alt text for your avatar
+    author: `My Name`,
+    // Used for SEO
+    description: `My site description...`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
   },
 }
