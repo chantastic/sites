2212dbe28471fe34a68bf128e94ba60b177427f8
Michael Chan <mijoch@gmail.com>
2020-05-16 07:58:58 -0700

    add head to static blog index

--- chan.dev-next/pages/blog/index.js
+++ chan.dev-next/pages/blog/index.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>Static Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
