2f1d5dfe76b2a6bba2caa07ad7bc67fe4b1dfde7
Michael Chan <mijoch@gmail.com>
2020-05-16 09:20:05 -0700

    create links on homepage

--- chan.dev-next/pages/blog/[id].js
+++ chan.dev-next/pages/blog/[id].js
@@ -1,6 +1,8 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
@@ -8,15 +10,15 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
-  );
+  )
 }
 
 export async function getStaticPaths() {
--- chan.dev-next/pages/index.js
+++ chan.dev-next/pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
-// import Link from "next/link";
+import Link from "next/link";
 import { getSortedPostsData } from "../lib/posts";
 import Layout from "../components/layout";
-import utilStyles from "../styles/utils.module.css"
+import utilStyles from "../styles/utils.module.css";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -23,11 +24,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/blog/[id]" as={`/blog/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
