cce23a87be26b534b2c90b8f89df3d8b9defb234
Michael Chan <mijoch@gmail.com>
2020-05-16 08:09:23 -0700

    add css module for layout

---
+++ chan.dev-next/components/blog-layout.js
@@ -0,0 +1,7 @@
+import styles from "./blog-layout.module.css";
+
+function Layout({ children }) {
+  return <div>{children}</div>;
+}
+
+export default Layout;
--- chan.dev-next/pages/blog/index.js
+++ chan.dev-next/pages/blog/index.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/blog-layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Static Post</title>
       </Head>
@@ -13,6 +14,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
