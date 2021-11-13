eaee04ec18b2c0ef96b5b720fbe62ad13aa489fa
Michael Chan <mijoch@gmail.com>
2021-11-13 07:32:30 -0800

    extract types to auto-loaded .d.ts file

--- reactpodcast.com/src/modules/paths.ts
+++ reactpodcast.com/src/modules/paths.ts
@@ -1,8 +1,3 @@
-type Episode = {
-	id: string;
-	title: string;
-};
-
-export function episodeSlug(episode: Episode): string {
+export function episodeSlug(episode: Pick<Episode, 'id' | 'title'>): string {
 	return `${episode.id}_${episode.title.replace(/[^a-zA-Z0-9]/g, '-')}`;
 }
