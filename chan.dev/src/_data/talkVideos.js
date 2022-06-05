68f55d8c726c415acedd140c714193538e476adf
Michael Chan <mijoch@gmail.com>
2022-06-04 20:04:31 -0400

    cleanup: update youtube url

--- chan.dev/src/_data/talkVideos.js
+++ chan.dev/src/_data/talkVideos.js
@@ -3,7 +3,7 @@ const Cache = require("@11ty/eleventy-cache-assets");
 module.exports = async function () {
   try {
     let videos = await Cache(
-      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLnc_NxpmOxaPBbn1PysP30GB0_8aQGnAa&key=${process.env.YOUTUBE_API_KEY}`,
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLnc_NxpmOxaPBbn1PysP30GB0_8aQGnAa&key=${process.env.YOUTUBE_API_KEY}`,
       {
         duration: "1d",
         type: "json",
