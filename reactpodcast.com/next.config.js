7048b8fb1e9295436c7dd6b78d0593b79e130720
Michael Chan <mijoch@gmail.com>
2022-06-24 01:34:08 -0600

    add redirects from episode numbers to /episode/:id

--- reactpodcast.com/next.config.js
+++ reactpodcast.com/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // https://nextjs.org/docs/api-reference/next.config.js/redirects
+  async redirects() {
+    return [
+      {
+        source: '/:slug(\\d{1,})',
+        destination: '/episodes/:slug',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 
