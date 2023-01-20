6c829349a04ae09b39f77383547d5e44a4cb860c
Michael Chan <mijoch@gmail.com>
2023-01-20 01:18:19 -0800

    npx astro add tailwind
    https://docs.astro.build/en/guides/integrations-guide/tailwind/

--- chan.dev-astro/astro.config.mjs
+++ chan.dev-astro/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: import.meta.env.DEV
     ? "http:localhost:3000"
     : "https://chan.dev",
+  integrations: [tailwind()],
 });
