26f38a691f51c5e723c5b342112e541521f08dff
Michael Chan <mijoch@gmail.com>
2023-01-19 23:36:18 -0800

    add posts collection items to RSS feed
    https://docs.astro.build/en/guides/rss/#using-content-collections

--- chan.dev-astro/src/pages/feed/rss-feed.xml.js
+++ chan.dev-astro/src/pages/feed/rss-feed.xml.js
@@ -1,11 +1,17 @@
 import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
-export function get() {
+export async function get() {
+  const posts = await getCollection("posts");
   return rss({
     title: "chan.dev",
     description: "chantastic tech",
     site: "https://chan.dev/",
-    items: [],
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.publishDate,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
