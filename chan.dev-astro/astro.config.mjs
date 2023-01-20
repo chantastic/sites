e76a982bfa5180a17bb9bc7c9c86373d0692435e
Michael Chan <mijoch@gmail.com>
2023-01-20 00:52:23 -0800

    Use env variables to change site value locally

--- chan.dev-astro/astro.config.mjs
+++ chan.dev-astro/astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://chan.dev",
+  site: import.meta.env.DEV
+    ? "http:localhost:3000"
+    : "https://chan.dev",
 });
