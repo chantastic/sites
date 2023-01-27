66e78ade3a46402346f3c972f700761521363b49
Michael Chan <mijoch@gmail.com>
2023-01-27 10:09:30 -0800

    move /feed/rss-feed.xml to /feed/rss.xml

--- chan.dev-astro/src/pages/feed/rss.xml.js
+++ chan.dev-astro/src/pages/feed/rss.xml.js
@@ -1,17 +1,12 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getPostsCollection } from "@modules/post.mjs";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
 
 const parser = new MarkdownIt();
 
 export async function get(context) {
-  const posts = await getCollection("posts", ({ data }) => {
-    if (data.publishDate) {
-      return true;
-    }
-    return false;
-  });
+  let posts = await getPostsCollection();
 
   return rss({
     title: "chan.dev",
