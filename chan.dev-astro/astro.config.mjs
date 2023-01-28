6273cc3e7c6636b2254e76a6e401b6e2e0ab35f0
Michael Chan <mijoch@gmail.com>
2023-01-28 12:47:46 -0800

    add remark-toc with addition of 'outline|content'

--- chan.dev-astro/astro.config.mjs
+++ chan.dev-astro/astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import remark_toc from "remark-toc";
 
 export default defineConfig({
-  site: import.meta.env.DEV ? "http:localhost:3000" : "https://chan.dev",
-  integrations: [tailwind(), sitemap()]
+  site: import.meta.env.DEV
+    ? "http:localhost:3000"
+    : "https://chan.dev",
+  integrations: [tailwind(), sitemap()],
+  markdown: {
+    remarkPlugins: [
+      [
+        remark_toc,
+        {
+          maxDepth: 2,
+          heading: "toc|outline|contents|table[ -]of[ -]contents?",
+        },
+      ],
+    ], // https://github.com/remarkjs/remark-toc
+  },
 });
