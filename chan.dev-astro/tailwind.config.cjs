7a1e0921e70847c324a62205b75f389db86785d2
Michael Chan <mijoch@gmail.com>
2023-03-17 16:19:55 -0700

    add talks, using tailwind aspect ratio plugin

--- chan.dev-astro/tailwind.config.cjs
+++ chan.dev-astro/tailwind.config.cjs
@@ -7,5 +7,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
