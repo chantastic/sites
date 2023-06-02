957e1b39cb0659bd487be4b6d07626c3cbf63d49
Michael Chan <mijoch@gmail.com>
2023-06-02 12:12:13 -0700

    add working tailwind theme for kbd and dl

--- chan.dev-astro/tailwind.config.cjs
+++ chan.dev-astro/tailwind.config.cjs
@@ -5,7 +5,42 @@ module.exports = {
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            kbd: {
+              backgroundColor: "#eee",
+              borderRadius: "3px",
+              border: "1px solid #b4b4b4",
+              boxShadow:
+                "0 1px 1px rgba(0, 0, 0, 0.2), 0 2px 0 0 rgba(255, 255, 255, 0.7) inset",
+              color: "#333",
+              display: "inline-block",
+              fontSize: "0.85em",
+              fontWeight: "700",
+              lineHeight: "1",
+              padding: "2px 4px",
+              whiteSpace: "nowrap",
+            },
+            dl: {
+              display: "flex",
+              flexFlow: "row wrap",
+              border: "1px solid currentColor",
+              padding: "1rem",
+            },
+            dt: {
+              flexBasis: "20%",
+              textAlign: "start",
+            },
+            dd: {
+              flexBasis: "70%",
+              flexGrow: 1,
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
