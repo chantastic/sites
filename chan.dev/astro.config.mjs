afa7d7a7f59cbd652d38c36319307a20eb10df0e
Michael Chan <mijoch@gmail.com>
2023-07-19 19:31:14 -0700

    add remark-directive

--- chan.dev/astro.config.mjs
+++ chan.dev/astro.config.mjs
@@ -10,101 +10,157 @@ import remark_external_links from "remark-external-links";
 import remark_embedder from "@remark-embedder/core";
 import oembed_transformer from "@remark-embedder/transformer-oembed";
 import remark_obsidian_callout from "remark-obsidian-callout";
+import remark_directive from "remark-directive";
+import { visit } from "unist-util-visit";
 
 // exclude collection files.
 // astro does this when building but not sitemap
 const excludedPaths = [
-	"decisions/decisions",
-	"lessons/lessons",
-	"os/os",
-	"posts/posts",
-	"uses/uses",
+  "decisions/decisions",
+  "lessons/lessons",
+  "os/os",
+  "posts/posts",
+  "uses/uses",
 ];
+
+function process_remark_directives() {
+  // note: this function acts mutably
+  return (tree) => {
+    visit(tree, (node) => {
+      if (
+        !(
+          node.type === "textDirective" ||
+          node.type === "leafDirective" ||
+          node.type === "containerDirective"
+        )
+      ) {
+        return;
+      }
+
+      const data = node.data || (node.data = {});
+      const hast = h(node.name, node.attributes);
+
+      switch (node.type) {
+        case "containerDirective": {
+          switch (node.name) {
+            case "hidden-script": {
+              data.hName = "section";
+              data.hProperties = {
+                className: "hidden-video-script",
+                ...hast.properties,
+              };
+
+              break;
+            }
+
+            default: {
+              data.hName = hast.tagName;
+              data.hProperties = hast.properties;
+
+              break;
+            }
+          }
+
+          break;
+        }
+
+        default: {
+          data.hName = hast.tagName;
+          data.hProperties = hast.properties;
+
+          break;
+        }
+      }
+    });
+  };
+}
 
 export default defineConfig({
 	site: import.meta.env.DEV ? "http://localhost:3000" : "https://chan.dev",
-	integrations: [
-		tailwind(),
-		sitemap({
-			filter: (page) =>
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) =>
 				excludedPaths.filter((path) => page.includes(path)).length ===
 				0,
-		}),
-	],
-	experimental: {
-		assets: true,
-	},
-	markdown: {
-		remarkPlugins: [
-			[
-				// https://github.com/remarkjs/remark-toc
-				remark_toc,
-				{
-					maxDepth: 2,
+    }),
+  ],
+  experimental: {
+    assets: true,
+  },
+  markdown: {
+    remarkPlugins: [
+      [
+        // https://github.com/remarkjs/remark-toc
+        remark_toc,
+        {
+          maxDepth: 2,
 					heading: "toc|outline|contents|table[ -]of[ -]contents?",
-				},
-			],
-			remark_deflist,
+        },
+      ],
+      remark_deflist,
       // [
       //   remark_obsidian,
       //   { markdownFolder: `${process.cwd()}/src/content` },
       // ],
-			[
-				remark_embedder,
-				{
-					transformers: [oembed_transformer],
-				},
-			],
-			// https://www.reliablesoft.net/noreferrer-noopener/#noreferrer-vs-nofollow
-			[remark_external_links, { rel: "noopener" }],
-			remark_obsidian_callout,
-		],
-		rehypePlugins: [
-			// https://docs.astro.build/en/guides/markdown-content/#heading-ids-and-plugins
-			rehypeHeadingIds,
-			[
-				rehype_autolink_headings,
-				{
-					behavior: "append",
-					properties: {
-						className: "heading--direct-link",
-					},
-					group: h("span", {
-						tabIndex: "-1",
-					}),
-					content: (element) => [
-						h(
-							"span",
-							{
-								className: "heading--direct-link-icon",
-								ariaHidden: true,
-							},
-							s(
-								"svg",
-								{
-									width: 16,
-									height: 16,
-									version: "1.1",
-									viewBox: "0 0 16 16",
-									xlmns: "http://www.w3.org/2000/svg",
-								},
-								[
-									s("path", {
-										fillrule: "evenodd",
-										fill: "currentcolor",
-										d: "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z",
-									}),
-								]
-							)
-						),
-						h(
-							"span",
-							{ "is:raw": true, className: "sr-only" },
-							`Section titled ${element.children[0].value}`
-						),
-					],
-				},
-			],
-		],
-	},
+      [
+        remark_embedder,
+        {
+          transformers: [oembed_transformer],
+        },
+      ],
+      // https://www.reliablesoft.net/noreferrer-noopener/#noreferrer-vs-nofollow
+      [remark_external_links, { rel: "noopener" }],
+      remark_obsidian_callout,
+      remark_directive,
+      process_remark_directives,
+    ],
+    rehypePlugins: [
+      // https://docs.astro.build/en/guides/markdown-content/#heading-ids-and-plugins
+      rehypeHeadingIds,
+      [
+        rehype_autolink_headings,
+        {
+          behavior: "append",
+          properties: {
+            className: "heading--direct-link",
+          },
+          group: h("span", {
+            tabIndex: "-1",
+          }),
+          content: (element) => [
+            h(
+              "span",
+              {
+                className: "heading--direct-link-icon",
+                ariaHidden: true,
+              },
+              s(
+                "svg",
+                {
+                  width: 16,
+                  height: 16,
+                  version: "1.1",
+                  viewBox: "0 0 16 16",
+                  xlmns: "http://www.w3.org/2000/svg",
+                },
+                [
+                  s("path", {
+                    fillrule: "evenodd",
+                    fill: "currentcolor",
+                    d: "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z",
+                  }),
+                ]
+              )
+            ),
+            h(
+              "span",
+              { "is:raw": true, className: "sr-only" },
+              `Section titled ${element.children[0].value}`
+            ),
+          ],
+        },
+      ],
+    ],
+  },
 });
---
+++ chan.dev/src/content/lessons/marchdown/remark-directive.md
@@ -0,0 +1,244 @@
+---
+title: 'Create Markdown "Components" with Remark Directives'
+date: 2023-07-13
+---
+
+## Notes
+
+Incompatible with obsidian plugin.
+
+## API reference
+
+`textDirective`
+
+```
+:i[the content]{id="content"}
+```
+
+`leafDirective`
+
+```
+::hr
+::br
+```
+
+supports `{key="value"}`?
+
+`containerDirective`
+
+:::my-container
+
+My contained content.
+:::
+
+---
+
+I love markdown but wish it had components.
+
+"That's what [MDX][] is for, right??"
+
+Not really. MDX is JavaScript file format, not markdown.[^1]
+What I want is a way to abstract repeated markup for embeds, intent links, wordy markup, and styled containers.
+
+[^1]:
+    Try and explain markdown with 1 layer of abstraction removed.
+
+    **JSX**: An XML syntax extension to JavaScript.
+
+    **Markdown**: A suprset of HTML
+
+    **MDX**: An XML syntax extension in a superset of HTML — in JavaScript.
+
+    WAT‽
+
+    There are too many layers of interpretation. My preference is to use JSX _or_ MD. And selectively parse markdown content thru a markdown processor.
+
+[MDX]: https://mdxjs.com/ "MDX documentation"
+
+## Enter Remark Directive
+
+[Remark directive][] is a [Remark][] plugin that introduces a customizeable syntax.
+
+This is the full syntax sample shown in documentation:
+
+```markdown
+
+```
+
+Let's break that down into the practical use cases shown above.
+
+[remark]: https://remark.js.org/ "Remark: markdown processor powered by plugins"
+
+### Details element
+
+```markdown
+<details>
+    <summary>Details</summary>
+    Something small enough to escape casual notice.
+</details>
+```
+
+```markdown
+:::my-container
+
+Any _markdown_ I want.
+:::
+```
+
+And here's a more complex example:
+
+## What I'd like
+
+I just want a few repeatable snippets for:
+
+- embeds
+- "bug me on social" intent links
+- hidden content[^2]
+
+[^2]: There are other good reasons to hide content.
+
+    Add the #body or #nerd url target to urls on [chan.dev][] and you'll see hidden debugging details, stats, and scripts appear.
+
+## Visually Hidden example
+
+:::my-component
+
+This is a test
+:::
+
+```markdown
+:::visually-hidden
+
+This description makes visible content accessible via assistive technologies.
+:::
+```
+
+oeunth :i[lovely] oeunth
+
+:::main{#readme}
+
+Lorem:br
+ipsum.
+
+::hr{.red}
+
+A :i[lovely] language know as :abbr[HTML]{title="HyperText Markup Language"}.
+
+:::
+
+```css
+.visually-hidden:not(:focus):not(:active) {
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+
+/*
+ * https://www.a11yproject.com/posts/how-to-hide-content/
+ * Dave Rupert @davatron5000
+ */
+```
+
+## Why markdown is important
+
+For the past 3 years, I used [eleventy][] to build [chan.dev][]. eleventy was my favorite static site generator before [Astro 2.0][Astro].
+
+But when I went to migrate all of my markdown content, I ran into a problem. It was littlered with [eleventy short codes][].
+Some pages probably still are!
+
+This is a very eleventy-specific way to create dynamic elements. And it couldn't go with me.
+
+[Eleventy]: https://www.11ty.dev/ "Eleventy, a simpler static site generator"
+[chan.dev]: htps://chan.dev "the chaotic digital garden of chantastic"
+[Astro]: https://astro.build/blog/astro-2/ "Astro 2.0 announcement post"
+[Eleventy shortcodes]: https://www.11ty.dev/docs/shortcodes/ "Template Language custom tags"
+
+## Enter Remark Directive
+
+[Remark directive][]
+
+[remark directive]: https://github.com/remarkjs/remark-directive
+
+To process the text content as markdown, there needs to be a newline separating the opening `div` and the markdown text.
+Again, very cool that
+
+The Markdown parser needs one preceeding line of whitespace, it will just spit out normal text.
+
+<!-- ![examples]() -->
+
+And it doesn't end there.
+Most AST-based markdown formatters will correct that to have new lines above and below the markdown.
+And this introduces another problem
+
+<!-- prettier-ignore-start -->
+```markdown
+<section>
+  <div>
+    <div>
+
+A _liiiiittle_ markdown.
+  </div>
+  </div>
+</section>
+```
+
+```markdown
+<div class="flex gap-x-3">
+  <div class="w-1/2">
+
+A _liiiiittle_ markdown.
+  </div>
+  <div class="w-1/2">
+
+Some **other** markdown.
+  </div>
+</div>
+```
+<!-- prettier-ignore-end -->
+
+<div class="flex gap-x-3">
+  <div class="w-1/2">
+
+A _liiiiittle_ markdown.
+
+  </div>
+  <div class="w-1/2">
+
+Some **other** markdown.
+
+  </div>
+</div>
+
+It's great that I can just drop in a [tailwindcss flex recipe](https://v1.tailwindcss.com/components/flexbox-grids "tailwindcss flexbox grids documentation and recipes"). But do you see what I mean by wierd?
+
+Anything that I want parsed as markdown requires surronding newlines.
+And that's not all!
+
+<!-- prettier-ignore-start -->
+
+```markdown
+<div class="flex gap-x-3">
+  <div class="w-1/2">
+    <div>
+
+A _liiiiittle_ markdown.
+
+  <div>
+  </div>
+</div>
+```
+
+<div class="flex gap-x-3">
+  <div class="w-1/2">
+    <div>
+
+A _liiiiittle_ markdown.
+
+  <div>
+  </div>
+</div>
+<!-- prettier-ignore-end -->
