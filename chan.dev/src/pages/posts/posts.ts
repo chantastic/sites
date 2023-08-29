949331ee2beddab202c5172b66cf9ec7af0ac7d1
Michael Chan <mijoch@gmail.com>
2023-08-29 12:50:28 -0700

    Update posts collection to include cover #til
    https://docs.astro.build/en/guides/assets/#update-content-collections-schemas

--- chan.dev/src/pages/posts/posts.ts
+++ chan.dev/src/pages/posts/posts.ts
@@ -5,10 +5,7 @@ import { z, reference, defineCollection } from "astro:content";
 export const COLLECTION_NAME = "posts";
 
 export function path(...segments: string[]) {
-  return COLLECTION.constructPathFromSegments(
-    COLLECTION_NAME,
-    ...segments
-  );
+  return COLLECTION.constructPathFromSegments(COLLECTION_NAME, ...segments);
 }
 
 export type CollectionEntry = ASTRO_CONTENT.CollectionEntry<
@@ -23,36 +20,33 @@ export async function getCollection(
     return false;
   },
   sort = (a: CollectionEntry, b: CollectionEntry) =>
-    COLLECTION.compareByDate(
-      a.data.publishDate,
-      b.data.publishDate
-    )
+    COLLECTION.compareByDate(a.data.publishDate, b.data.publishDate)
 ) {
-  const result = await ASTRO_CONTENT.getCollection(
-    COLLECTION_NAME,
-    filter
-  );
+  const result = await ASTRO_CONTENT.getCollection(COLLECTION_NAME, filter);
 
   return result.sort(sort);
 }
 
 export const collectionSchema = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    publishDate: z.date().optional(),
-    description: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    tldr: z.string().optional(),
-    references: z.array(z.string().url()).optional(),
-    shoutouts: z.array(reference("entities")).optional(),
-    og: z
-      .object({
-        title: z.string().optional(),
-        audio: z.string().url().optional(),
-        image: z.string().url().optional(),
-      })
-      .optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      publishDate: z.date().optional(),
+      description: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      tldr: z.string().optional(),
+      references: z.array(z.string().url()).optional(),
+      shoutouts: z.array(reference("entities")).optional(),
+      cover: image().optional(),
+      coverAlt: z.string().optional(),
+      og: z
+        .object({
+          title: z.string().optional(),
+          audio: z.string().url().optional(),
+          image: z.string().url().optional(),
+        })
+        .optional(),
+    }),
 });
 
 export function entryHasTags(entry: CollectionEntry) {
