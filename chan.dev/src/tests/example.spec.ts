6b698717c27c0ceff1764963264c1b01345d7a7e
Michael Chan <mijoch@gmail.com>
2023-08-31 20:04:35 -0700

    configure playwright #til
    https://docs.astro.build/en/guides/testing/#advanced-launching-a-development-web-server-during-the-tests

--- chan.dev/src/tests/example.spec.ts
+++ chan.dev/src/tests/example.spec.ts
@@ -1,18 +1,27 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+import { test, expect } from "@playwright/test";
+
+test("meta is correct", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page).toHaveTitle(
+    "The incoherant ramblings of chantastic. | chan.dev"
+  );
 });
