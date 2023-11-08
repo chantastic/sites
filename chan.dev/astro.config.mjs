8bd4d99b3578773074cc02adb3594ba0c26078f0
Michael Chan <mijoch@gmail.com>
2023-11-08 11:40:32 -0800

    format astro.config to make incomnig changes vis.

--- chan.dev/astro.config.mjs
+++ chan.dev/astro.config.mjs
@@ -1,91 +1,88 @@
-import { defineConfig, sharpImageService } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import remark_toc from "remark-toc";
-import remark_deflist from "remark-deflist";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-import rehype_autolink_headings from "rehype-autolink-headings";
-import { h, s } from "hastscript";
-import remark_external_links from "remark-external-links";
-import remark_embedder from "@remark-embedder/core";
-import oembed_transformer from "@remark-embedder/transformer-oembed";
-import remark_obsidian_callout from "remark-obsidian-callout";
-import remark_directive from "remark-directive";
-import { visit } from "unist-util-visit";
-import astro_expressive_code from "astro-expressive-code";
-import react from "@astrojs/react";
+import {defineConfig, sharpImageService} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import remark_toc from 'remark-toc'
+import remark_deflist from 'remark-deflist'
+import {rehypeHeadingIds} from '@astrojs/markdown-remark'
+import rehype_autolink_headings from 'rehype-autolink-headings'
+import {h, s} from 'hastscript'
+import remark_external_links from 'remark-external-links'
+import remark_embedder from '@remark-embedder/core'
+import oembed_transformer from '@remark-embedder/transformer-oembed'
+import remark_obsidian_callout from 'remark-obsidian-callout'
+import remark_directive from 'remark-directive'
+import {visit} from 'unist-util-visit'
+import astro_expressive_code from 'astro-expressive-code'
+import react from '@astrojs/react'
+import netlify from '@astrojs/netlify/functions'
 function process_remark_directives() {
   // note: this function acts mutably
   return (tree) => {
     visit(tree, (node) => {
       if (
         !(
-          node.type === "textDirective" ||
-          node.type === "leafDirective" ||
-          node.type === "containerDirective"
+					node.type === 'textDirective' ||
+					node.type === 'leafDirective' ||
+					node.type === 'containerDirective'
         )
       ) {
-        return;
-      }
-
-      const data = node.data || (node.data = {});
-      const hast = h(node.name, node.attributes);
-
+				return
+			}
+			const data = node.data || (node.data = {})
+			const hast = h(node.name, node.attributes)
       switch (node.type) {
-        case "containerDirective": {
+				case 'containerDirective': {
           switch (node.name) {
-            case "hidden-script": {
-              data.hName = "section";
+						case 'hidden-script': {
+							data.hName = 'section'
               data.hProperties = {
-                className: "hidden-video-script",
+								className: 'hidden-video-script',
                 ...hast.properties,
-              };
-
-              break;
+							}
+							break
             }
-
             default: {
-              data.hName = hast.tagName;
-              data.hProperties = hast.properties;
-
-              break;
+							data.hName = hast.tagName
+							data.hProperties = hast.properties
+							break
             }
           }
-
-          break;
+					break
         }
-
         default: {
-          data.hName = hast.tagName;
-          data.hProperties = hast.properties;
-
-          break;
+					data.hName = hast.tagName
+					data.hProperties = hast.properties
+					break
         }
       }
-    });
-  };
+		})
+	}
 }
-
 function exchange_relative_links_with_absolute() {
   return (tree) => {
     visit(tree, (node) => {
       if (
-        ["link", "definition"].includes(node?.type) &&
-        node.url.startsWith("/")
+				['link', 'definition'].includes(node?.type) &&
+				node.url.startsWith('/')
       ) {
-        node.url = new URL(node.url, site).toString();
+				node.url = new URL(node.url, site).toString()
+			}
+		})
       }
-    });
-  };
 }
-
-const site = import.meta.env.DEV ? "http://localhost:4321" : "https://chan.dev";
+const site = import.meta.env.DEV
+	? 'http://localhost:4321'
+	: 'https://chan.dev'
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 2426 },
+	server: {
+		port: 2426,
+	},
   vite: {
-    optimizeDeps: { exclude: ["fsevents"] },
+		optimizeDeps: {
+			exclude: ['fsevents'],
+		},
   },
   site,
   integrations: [
@@ -93,12 +90,12 @@ export default defineConfig({
     sitemap({
       // I'm building dedicated share pages that I don't want indexed.
       // Ultimately, this may be better as a dynamic route.
-      filter: (page) => !page.endsWith("/share/"),
+			filter: (page) => !page.endsWith('/share/'),
     }),
     astro_expressive_code({
       frames: {
         styleOverrides: {
-          frameBoxShadowCssValue: "none",
+					frameBoxShadowCssValue: 'none',
         },
       },
     }),
@@ -115,7 +112,8 @@ export default defineConfig({
         remark_toc,
         {
           maxDepth: 2,
-          heading: "toc|outline|contents|table[ -]of[ -]contents?",
+					heading:
+						'toc|outline|contents|table[ -]of[ -]contents?',
         },
       ],
       remark_deflist,
@@ -129,7 +127,7 @@ export default defineConfig({
       [
         remark_external_links,
         {
-          rel: "noopener",
+					rel: 'noopener',
         },
       ],
       remark_obsidian_callout,
@@ -142,34 +140,34 @@ export default defineConfig({
       [
         rehype_autolink_headings,
         {
-          behavior: "append",
+					behavior: 'append',
           properties: {
-            className: "heading--direct-link",
+						className: 'heading--direct-link',
           },
-          group: h("span", {
-            tabIndex: "-1",
+					group: h('span', {
+						tabIndex: '-1',
           }),
           content: (element) => [
             h(
-              "span",
+							'span',
               {
-                className: "heading--direct-link-icon",
+								className: 'heading--direct-link-icon',
                 ariaHidden: true,
               },
               s(
-                "svg",
+								'svg',
                 {
                   width: 16,
                   height: 16,
-                  version: "1.1",
-                  viewBox: "0 0 16 16",
-                  xlmns: "http://www.w3.org/2000/svg",
+									version: '1.1',
+									viewBox: '0 0 16 16',
+									xlmns: 'http://www.w3.org/2000/svg',
                 },
                 [
-                  s("path", {
-                    fillrule: "evenodd",
-                    fill: "currentcolor",
-                    d: "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z",
+									s('path', {
+										fillrule: 'evenodd',
+										fill: 'currentcolor',
+										d: 'M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z',
                   }),
                 ]
               )
