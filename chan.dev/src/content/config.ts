b3db8c91388335354f4e9db3f62c8b79817ce432
Michael Chan <mijoch@gmail.com>
2024-02-12 17:06:34 -0800

    add /projects pages structure

--- chan.dev/src/content/config.ts
+++ chan.dev/src/content/config.ts
@@ -2,6 +2,7 @@ import {collectionSchema as decisions} from '#pages/decisions/decisions'
 import {collectionSchema as lessons} from '#pages/lessons/lessons'
 import {collectionSchema as os} from '#pages/os/os'
 import {collectionSchema as posts} from '#pages/posts/posts'
+import {collectionSchema as projects} from '#pages/projects/projects'
 import {collectionSchema as uses} from '#pages/uses/uses'
 import {collectionSchema as entities} from '#pages/entities/entities'
 
@@ -10,6 +11,7 @@ export const collections = {
 	decisions,
 	lessons,
 	os,
+	projects,
 	uses,
 	entities,
 }
---
+++ chan.dev/src/pages/projects/projects.ts
@@ -0,0 +1,42 @@
+import * as ASTRO_CONTENT from 'astro:content'
+import * as COLLECTION from '#modules/collection'
+import {z, defineCollection} from 'astro:content'
+
+export const COLLECTION_NAME = 'projects'
+
+export function path(...segments: string[]) {
+	return COLLECTION.constructPathFromSegments(
+		COLLECTION_NAME,
+		...segments
+	)
+}
+
+export function title(specific?: string) {
+	return specific
+		? specific
+		: COLLECTION.prefixCollectionTitle(COLLECTION_NAME)
+}
+
+export type CollectionEntry = ASTRO_CONTENT.CollectionEntry<
+	typeof COLLECTION_NAME
+>
+
+export async function getCollection(
+	filter?: (entry: CollectionEntry) => unknown,
+	sort = (a: CollectionEntry, b: CollectionEntry) =>
+		Number(b.id.slice(0, 4)) - Number(a.id.slice(0, 4))
+) {
+	const result = await ASTRO_CONTENT.getCollection(
+		COLLECTION_NAME,
+		filter
+	)
+
+	return result.sort(sort)
+}
+
+export const collectionSchema = defineCollection({
+	schema: z.object({
+		status: z.string(),
+		effects: z.array(z.string()),
+	}),
+})
