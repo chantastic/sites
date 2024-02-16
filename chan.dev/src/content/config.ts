b9cd3f62675beef6975278698d7b0b704a46883a
Michael Chan <mijoch@gmail.com>
2024-02-16 13:00:35 -0800

    alphabetize imports and exports in content config.ts

--- chan.dev/src/content/config.ts
+++ chan.dev/src/content/config.ts
@@ -1,19 +1,21 @@
+import {collectionSchema as dailies} from '#content/dailies/_dailies'
 import {collectionSchema as decisions} from '#content/decisions/_decisions'
+import {collectionSchema as entities} from '#content/entities/_entities'
+import {collectionSchema as guide_steps} from '#content/guide_steps/_guide_steps'
 import {collectionSchema as lessons} from '#content/lessons/_lessons'
 import {collectionSchema as os} from '#content/os/_os'
 import {collectionSchema as posts} from '#content/posts/_posts'
 import {collectionSchema as projects} from '#content/projects/_projects'
 import {collectionSchema as uses} from '#content/uses/_uses'
-import {collectionSchema as entities} from '#content/entities/_entities'
-import {collectionSchema as guide_steps} from '#content/guide_steps/_guide_steps'
 
 export const collections = {
-	posts,
+	dailies,
 	decisions,
-	lessons,
-	os,
-	projects,
-	uses,
 	entities,
 	guide_steps,
+	lessons,
+	os,
+	posts,
+	projects,
+	uses,
 }
