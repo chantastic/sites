321526a3f549df979450f3f9b4522e824628e820
Michael Chan <mijoch@gmail.com>
2024-07-08 21:26:50 -0700

    does making it SSR fix the download issue?

--- chan.dev/src/pages/auth/index.js
+++ chan.dev/src/pages/auth/index.js
@@ -1,6 +1,6 @@
 import {WorkOS} from '@workos-inc/node'
 // import type {APIRoute} from 'astro'
-// export const prerender = false
+export const prerender = false
 
 const workos = new WorkOS(import.meta.env.WORKOS_API_KEY)
 const clientId = import.meta.env.WORKOS_CLIENT_ID
