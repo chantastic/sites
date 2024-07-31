dfcf853bb4f4ceb83dc034b3b0c8ab4799c4a02a
Michael Chan <mijoch@gmail.com>
2024-07-30 18:01:35 -0700

    add sign-out route that deletes cookie and closes session
    - create sign-out APIRoute
    - link to it
    - dismiss prefetch requests
    - get session and construct logoutUrl
    - logout
    - create reusabel component with data-astro-prefetch="false" to attempt to bypass prefetching altogether

---
+++ chan.dev/src/pages/sign-out.ts
@@ -0,0 +1,66 @@
+import {WorkOS} from '@workos-inc/node'
+import * as AUTHKIT from '#lib/authkit'
+import type {APIRoute} from 'astro'
+import {createRemoteJWKSet, jwtVerify} from 'jose'
+
+const workos = new WorkOS(import.meta.env.WORKOS_API_KEY)
+
+export const GET: APIRoute = async ({
+	redirect,
+	cookies,
+	request,
+}) => {
+	if (isPrefetchRequest(request)) {
+		return new Response('ok')
+	}
+
+	try {
+		let session = await AUTHKIT.getSessionFromCookie(
+			cookies.get(AUTHKIT.COOKIE_NAME)!
+		)
+
+		const JWKS = createRemoteJWKSet(
+			new URL(
+				workos.userManagement.getJwksUrl(
+					import.meta.env.WORKOS_CLIENT_ID
+				)
+			)
+		)
+
+		const verifiedSession = await jwtVerify(
+			session!.accessToken,
+			JWKS
+		)
+
+		const logoutRedirect = workos.userManagement.getLogoutUrl({
+			sessionId: verifiedSession.payload.sid,
+		})
+
+		cookies.delete(AUTHKIT.COOKIE_NAME, AUTHKIT.COOKIE_OPTIONS)
+		return redirect(logoutRedirect)
+	} catch (e) {
+		cookies.delete(AUTHKIT.COOKIE_NAME, AUTHKIT.COOKIE_OPTIONS)
+		return redirect('/')
+	}
+}
+
+function isPrefetchRequest(request: Request) {
+	if (
+		request.headers.get('Purpose') === 'prefetch' ||
+		request.headers.get('Sec-Purpose') === 'prefetch'
+	) {
+		return true
+	}
+
+	if (request.headers.get('X-Prefetch') === 'true') {
+		return true
+	}
+
+	if (request.headers.get('User-Agent')?.includes('prefetch')) {
+		return true
+	}
+
+	return false
+}
+
+export const prerender = false
