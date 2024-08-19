761444f26335d73a3c01f9b7276af55b152f12be
Michael Chan <mijoch@gmail.com>
2024-08-19 15:32:53 -0700

    omg. this feature is driving me nuts

--- chan.dev/src/pages/sign-in.ts
+++ chan.dev/src/pages/sign-in.ts
@@ -4,7 +4,6 @@ import type {APIRoute} from 'astro'
 export const GET: APIRoute = async ({redirect}) => {
 	const authorizationUrl = AUTHKIT.getAuthorizationUrl()
 
-	return new Response(authorizationUrl.toString())
 	return redirect(authorizationUrl)
 }
 
