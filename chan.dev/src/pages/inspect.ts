2d3b274a04d58f6ef41b5c84a889fa11002f0dc6
Michael Chan <mijoch@gmail.com>
2024-08-19 14:42:42 -0700

    display CF_PAGES_URL dynamic value

--- chan.dev/src/pages/inspect.ts
+++ chan.dev/src/pages/inspect.ts
@@ -1,11 +1,20 @@
 import * as AUTHKIT from '#lib/authkit'
 import type {APIRoute} from 'astro'
 
-export const GET: APIRoute = async ({redirect}) => {
+export const GET: APIRoute = async () => {
 	const authorizationUrl = AUTHKIT.getAuthorizationUrl()
 
-	console.log(authorizationUrl)
-	return new Response(authorizationUrl)
+	return new Response(
+		JSON.stringify({
+			authorizationUrl,
+			CF_PAGES_URL: import.meta.env.CF_PAGES_URL,
+		}),
+		{
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	)
 }
 
 export const prerender = false
